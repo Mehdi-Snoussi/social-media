c4eb111ff925bd6074920a436b5b49ff7ffb87ba
Snoussi Mehdi <93842139+Mehdi-Snoussi@users.noreply.github.com>
2025-01-16 00:52:29 +0100

    fix: login navbar

--- src/app/core/interceptors/social.interceptor.ts
+++ src/app/core/interceptors/social.interceptor.ts
@@ -3,16 +3,56 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpResponse,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class SocialInterceptor implements HttpInterceptor {
+  activeRequests = 0;
+  constructor(private authService: AuthService) {}
 
-  constructor() {}
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    if (this.activeRequests === 0) {
+      request = request.clone({
+        headers: request.headers.set('Content-Type', 'application/json'),
+      });
+      return new Observable((observer) => {
+        const subscription = next
+          .handle(this.addAuthorizationHeaderToRequest(request))
+          .subscribe({
+            next: () => {
+              next.handle(request);
+            },
+            error: (err) => {
+              if (navigator.onLine) console.error(err);
+              observer.error(err);
+            },
+          });
+        return () => {
+          subscription.unsubscribe();
+        };
+      });
+    }
+    this.activeRequests++;
+    return next.handle(request).pipe();
+  }
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+  addAuthorizationHeaderToRequest(
+    request: HttpRequest<unknown>
+  ): HttpRequest<unknown> {
+    const token = this.authService.getTokenFromCookie();
+    if (!token) return request;
+
+    if (!request.url.includes('entreprise')) {
+      return request.clone({
+        headers: request.headers.set('Authorization', 'Bearer ' + token),
+      });
+    } else return request;
   }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../core/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  isUserConnected = false;
+  constructor(private authService: AuthService) {
+    this.isUserConnected = this.authService.isAuthenticated();
+    console.log(this.authService.getUserConnectedInfo());
+  }
 
+  logout() {
+    this.authService.clearData()
+  }
 }
